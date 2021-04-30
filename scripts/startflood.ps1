25bc6278637102fb558fef3742d05d49399d4282
prannoyc <83410935+prannoyc@users.noreply.github.com>
2021-04-30 15:05:22 +0530

    Update startflood.ps1

--- scripts/startflood.ps1
+++ scripts/startflood.ps1
@@ -11,14 +11,13 @@
 #Declare some variables and input parameters
 $access_token = $env:MY_FLOOD_TOKEN
 $api_url = "https://api.flood.io"
-$data_path ='scripts/jmeter/jmeter_1000rpm.jmx'
+$data_path ='scripts/jmeter/MCI.csv'
 $script_path = 'scripts/jmeter/002_MCI.jmx'
 $flood_project = 'azure-devops'
 $flood_name = 'myAzureTest'
 
 #Setup the API URI that contains all parameters required to start a Grid, Flood and test settings.
 $uri = "$api_url/api/floods?flood[tool]=jmeter&flood[threads]=5&flood[duration]=120&flood[project]=$flood_project&flood[privacy]=public&flood[name]=$flood_name&flood[grids][][infrastructure]=demand&flood[grids][][instance_quantity]=1&flood[grids][][region]=us-east-1&flood[grids][][instance_type]=m5.xlarge&flood[grids][][stop_after]=10"
-
 
 
 #Encode the Flood auth token with Base64 and use it as a header for our request to Flood API
@@ -29,20 +28,51 @@ $headers = @{
     'Authorization' = $basicAuthValue
 }
 
-#Read the data file and transplant it as part of a UTF-8 based payload
+#Read the script file and transplant it as part of a UTF-8 based payload
 $fileBytes = [System.IO.File]::ReadAllBytes($data_path);
 $fileEnc = [System.Text.Encoding]::GetEncoding('UTF-8').GetString($fileBytes);
 $boundary = [System.Guid]::NewGuid().ToString();
 $LF = "`r`n";
-$contentType = "multipart/form-data; boundary1=`"$boundary`""
-$payload1 = (
+$contentType = "multipart/form-data; boundary=`"$boundary`""
+$payload = (
     "--$boundary",
-    "Content-Disposition: form-data; name=`"flood_files[]`"; filename=`"jmeter_1000rpm.jmx`"",
+    "Content-Disposition: form-data; name=`"flood_files[]`"; filename=`"MCI.csv`"",
     "Content-Type: application/octet-stream$LF",
-    $fileEnc1,
+    $fileEnc,
     "--$boundary--$LF"
 ) -join $LF
 
+
+#Submit the POST request to the Flood API and capture the returned Flood UUID
+#Store the Flood UUID as a variable that can be shared with other Azure Devops steps
+try {
+    $responseFull = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -ContentType $contentType -Body $payload
+    $outFloodID = $responseFull.uuid
+    Write-Output "Flood ID is: $outFloodID"
+    Write-Output "##vso[task.setvariable variable=flood_uuid;]$outFloodID"
+    
+}
+catch {
+    $responseBody = ""
+    $errorMessage = $_.Exception.Message
+    if (Get-Member -InputObject $_.Exception -Name 'Response') {
+        write-output $_.Exception.Response
+       
+        try {
+            $result = $_.Exception.Response.GetResponseStream()
+            $reader = New-Object System.IO.StreamReader($result, [System.Text.Encoding]::ASCII)
+            $reader.BaseStream.Position = 0
+            $reader.DiscardBufferedData()
+            $responseBody = $reader.ReadToEnd();
+            Write-Output "response body: $responseBody"
+        }
+        catch {
+            Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Cannot get more information."
+        }
+    }
+    Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Response body: $responseBody"
+
+}
 
 #Read the script file and transplant it as part of a UTF-8 based payload
 $fileBytes = [System.IO.File]::ReadAllBytes($script_path);
@@ -67,11 +97,6 @@ try {
     Write-Output "Flood ID is: $outFloodID"
     Write-Output "##vso[task.setvariable variable=flood_uuid;]$outFloodID"
     
-    $responseFull = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -ContentType $contentType -Body $payload1
-    $outFloodID = $responseFull.uuid
-    Write-Output "Flood ID is: $outFloodID"
-    Write-Output "##vso[task.setvariable variable=flood_uuid;]$outFloodID"
-
 }
 catch {
     $responseBody = ""
