e184c7783a765b59c137c5c4ee2177b285ea495c
prannoyc <83410935+prannoyc@users.noreply.github.com>
2021-05-04 01:49:09 +0530

    Update startflood.ps1

--- scripts/startflood.ps1
+++ scripts/startflood.ps1
@@ -29,9 +29,20 @@ $headers = @{
 
 #Read the data file and transplant it as part of a UTF-8 based payload
 
-$file = [io.file]::ReadAllBytes($data_path)
-[io.file]::WriteAllBytes('data/flood/files/MCI.csv',$file)
+$fileBytesOne = [System.IO.File]::ReadAllBytes($data_path);
+$fileEncOne = [System.Text.Encoding]::GetEncoding('UTF-8').GetString($fileBytesOne);
+$boundaryOne = [System.Guid]::NewGuid().ToString();
+$LF = "`r`n";
+$contentTypeOne = "multipart/form-data; boundary=`"$boundaryOne`""
+$payloadOne = (
+    "--$boundaryOne",
+    "Content-Disposition: form-data; name=`"flood_files[]`"; filename=`"MCI.csv`"",
+    "Content-Type: text/csv$LF",
+    $fileEncOne,
+    "--$boundaryOne--$LF"
+) -join $LF
 
+Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -ContentType $contentTypeOne -Body $payloadOne
 
 #Read the script file and transplant it as part of a UTF-8 based payload
 $fileBytes = [System.IO.File]::ReadAllBytes($script_path);
